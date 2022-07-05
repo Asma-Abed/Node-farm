b1883d8d01575c1b8876d6589f80bfdf4fde9907
Asma-Abed <abed.asma@hotmail.com>
2022-07-05 16:06:39 +0200

    module added

--- index.js
+++ index.js
@@ -1,47 +1,8 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const replaceTemplate = require('./modules/replaceTemplate');
 
-// reading and writing files
-// :::::::::::::::::::::::::::::::::::::::
-// sync
-// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-// console.log(textIn);
-// const textOut = `this is what avocado is: ${textIn}.\nCreated on ${Date.now()}`;
-// fs.writeFileSync('./txt/output.txt', textOut);
-// console.log('file written');
-
-// // async
-// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-//   if (err) return console.log('Error!');
-//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-//     console.log(data2);
-//     fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
-//       console.log(data3);
-//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
-//         console.log('file has been written');
-//       });
-//     });
-//   });
-// });
-// console.log('file is being read');
-
-// :::::::::::::::::::::::::::
-// server
-// ::::::::::::::::::::::::::::::::::::::::::
-const replaceTemplate = (temp, product) => {
-  let output = temp.replaceAll('{%PRODUCTNAME%}', product.productName);
-  output = output.replaceAll('{%IMAGE%}', product.image);
-  output = output.replaceAll('{%PRICE%}', product.price);
-  output = output.replaceAll('{%FROM%}', product.from);
-  output = output.replaceAll('{%NUTRIENTS%}', product.nutrients);
-  output = output.replaceAll('{%QUANTITY%}', product.quantity);
-  output = output.replaceAll('{%DESCRIPTION%}', product.description);
-  output = output.replaceAll('{%ID%}', product.id);
-  if (!product.organic)
-    output = output.replaceAll('{%NOT_ORGANIC%}', 'not-organic');
-  return output;
-};
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
   'utf-8'
